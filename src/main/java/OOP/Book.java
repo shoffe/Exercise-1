f2e38d8c3cd3bb77f6cd65406d6b3ad7274c06ad
shoffe <94527738+shoffe@users.noreply.github.com>
2024-03-15 20:34:50 +0700

    ready

--- src/main/java/OOP/Book.java
+++ src/main/java/OOP/Book.java
@@ -1,29 +1,53 @@
 package OOP;
 
 public class Book {
-    String name;
-    String author;
-    int yearPublic;
+    private String name;
+    private String author;
+    private int yearPublic;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublic() {
+        return yearPublic;
+    }
+
+    public void setYearPublic(int yearPublic) {
+        this.yearPublic = yearPublic;
+    }
 
     public Book(String name, String author, int yearPublic) {
         this.name = name;
         this.author = author;
         this.yearPublic = yearPublic;
     }
-
-    public static void printProperties(String name, String author, int yearPublic) {
-        System.out.println("Имя: " + name + ", Автор: " + author + ", Дата публикации: " + yearPublic);
-    }
-
-    public static void main(String[] args) {
-        Book shild = new Book("Java", "Shild", 2012);
-        Book magic = new Book("Магия", "Магистр Йода", 2013);
-        Book bober = new Book("Бобриная хата", "Бобёр!", 1999);
-
-        Book[] booksArray = {shild, magic, bober};
-
-        for (Book book : booksArray) {
-            book.printProperties(book.name, book.author, book.yearPublic);
-        }
+    public String toString() {
+        return "Имя: " + name + ", Автор: " + author + ", Дата публикации: " + yearPublic;
     }
 }
+    class Playbook {
+        public static void main(String[] args) {
+            Book shildBook = new Book("Java", "Shild", 2012);
+            Book magicBook = new Book("Магия", "Магистр Йода", 2013);
+            Book boberBook = new Book("Бобриная хата", "Бобёр!", 1999);
+
+            Book[] booksArray = {shildBook, magicBook, boberBook};
+
+            for (Book book : booksArray) {
+                    System.out.println(book);
+            }
+        }
+    }
--- src/main/java/OOP/Greeter.java
+++ src/main/java/OOP/Greeter.java
@@ -2,11 +2,10 @@ package OOP; // **Hello World with a Twist**:
 
 public class Greeter {
     public static void main(String[] args) {
-        Greeter g1 = new Greeter();
-        g1.greet("Саша");
-        g1.greet("Влад");
+        Greeter.greet("Саша");
+        Greeter.greet("Влад");
     }
    public static void greet(String name) {
-       System.out.println("Hello, "+ name);
+       System.out.println("Hello, " + name);
    }
 }
